497c5e73ca7f6f3961a4a5a05868958f20b3d836
vanlandschootsh900 <vanlandschootsh900@northwested.org>
2025-05-06 10:55:13 -0400

    s

--- main.py
+++ main.py
@@ -4,7 +4,8 @@
 
 import pygame
 import sys
-import config # Import the config module
+import config
+import random
 
 def init_game ():
 
@@ -23,22 +24,60 @@ def handle_events ():
             if event.key == pygame.K_ESCAPE:
                 return False
     return True
+class Circle:
+
+    def __init__(self,rad,color):
+        self.rad = rad
+        self.color = color
+
+        buffer=10
+
+        self.x = random.randint(self.rad +buffer,config.WINDOW_HEIGHT- self.rad-buffer)
+        self.y = random.randint(self.rad +buffer,config.WINDOW_HEIGHT- self.rad-buffer)
+        self.change_x = random.choice([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5])
+        self.change_y = random.choice([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5])
+    def move(self):
+        self.x += self.change_x
+        self.y += self.change_y
+
+        if self.x <= self.rad or self.x >= config.WINDOW_WIDTH - self.rad:
+            self.change_x *= -1
+
+        if self.y <= self.rad or self.y >= config.WINDOW_HEIGHT - self.rad:
+            self.change_y *= -1
+
+    def draw(self,screen):
+        pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.rad)
+
 
 def main():
     
     screen = init_game()
     clock = pygame.time.Clock()
 
-
+    circles = [
+        Circle(25, config.RED),
+        Circle(30, config.GREEN),
+        Circle(35, config.BLUE),
+        Circle(40, config.PURPLE)
+        ]
     airal = pygame.font.SysFont('airal',55)
-    
+
+
     running = True
     while running:
         running = handle_events()
-        screen.fill(config.WHITE) # Use color from config
-        
-        # Add code to draw stuff (for example) below this comment
 
+        for circle in circles:
+            circle.move()
+
+        screen.fill(config.WHITE) # Use color from config
+
+        for circle in circles:
+            circle.draw(screen)
+
+        # Add code to draw stuff (for example) below this comment
+  
 
 
 
